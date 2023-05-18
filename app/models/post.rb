cb46b008b56405042a0fabc7a83597039b6f5ce1
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-05-18 15:16:19 +0100

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_post_count
+  after_destroy :update_post_count
 
   def initialize(attributes = {})
     super
